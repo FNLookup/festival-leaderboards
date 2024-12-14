7fe4191de702a2944c9b8a2d6dca6757cfcd4613
tposejank <81495861+tposejank@users.noreply.github.com>
2024-12-13 21:39:25 -0500

    BRO

--- leaderboards.py
+++ leaderboards.py
@@ -315,6 +315,7 @@ def main():
 
 def getEG1Token(authParams, basicAuth):
     request = requests.post('https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token', data=authParams, headers={'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': f"Basic {basicAuth}"})
+    print(request.text)
     request.raise_for_status()
     json = request.json()
     #print(json)
